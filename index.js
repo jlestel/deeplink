11bf4fee86ffc26d2f87bf122dbfa7a0460f0fb8
Julien <julien@citio.digital>
2024-12-22 05:03:10 +0100

    up

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const getNativeProtocol = (url) => {
   if (domain.endsWith('linkedin.com')) return 'linkedin';
   if (domain.endsWith('spotify.com')) return 'spotify';
   if (domain.endsWith('leclercdrive.fr')) return 'leclercdrive';
-  if (domain.endsWith('e.leclerc')) return 'monleclerc';
+  if (domain.endsWith('e.leclerc')) return 'leclerc';
   return 'https'; // Default to HTTPS for unsupported domains
 };
 
